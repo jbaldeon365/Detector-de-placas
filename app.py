f39e71e3d4fe296d7d20ebb11ffaf61a55e7e643
josebaldeon365 <josebaldeon365@gmail.com>
2025-11-09 18:08:03 -0500

    version para render 6

--- app.py
+++ app.py
@@ -66,4 +66,5 @@ def upload_image():
         return jsonify({"success": False, "error": "No se detectaron placas."})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
