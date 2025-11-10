21fd92f1b2de714d6849558939414ddc5c650e5b
josebaldeon365 <josebaldeon365@gmail.com>
2025-11-09 19:21:55 -0500

    fix: bind gunicorn to render port 2

--- app.py
+++ app.py
@@ -66,5 +66,5 @@ def upload_image():
         return jsonify({"success": False, "error": "No se detectaron placas."})
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))  # Render recomienda 10000 como default
     app.run(host="0.0.0.0", port=port)
